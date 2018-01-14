a0e3b65c215ec469ee2217f76d2388b9fde14478
Bruce Hauman <bhauman@gmail.com>
2018-01-14 12:05:09 -0500

    add debug log for dev

---
+++ src/clj_readline/utils.clj
@@ -0,0 +1,16 @@
+(ns clj-readline.utils
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string  :as string]
+   [clojure.pprint  :as pp]))
+
+(def ^:dynamic *debug-log* true)
+
+(defn log [& args]
+  (when *debug-log*
+    (spit "debug-log" (string/join "\n" (map #(if (string? %)
+                                                (str % "\n")
+                                                (with-out-str (pp/pprint %)))
+                                             args))
+          :append true))
+  (last args))
