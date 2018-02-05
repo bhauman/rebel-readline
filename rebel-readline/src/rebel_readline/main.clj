de63107d4f3e3fb4ad5415873da33d5017f70ee3
Bruce Hauman <bhauman@gmail.com>
2018-02-05 10:25:32 -0500

    clean up main a little

--- rebel-readline/src/rebel_readline/main.clj
+++ rebel-readline/src/rebel_readline/main.clj
@@ -2,7 +2,7 @@
   (:require
    [rebel-readline.core :refer [line-reader clj-repl-read with-readline-input-stream]]
    [rebel-readline.service.impl.local-clojure-service :as local-clj-service]
-
+   
    [rebel-readline.service.impl.cljs-service :as cljs-service]   
    [cljs.repl.nashorn :as nash]
    [cljs.repl :as cljs-repl]
@@ -10,27 +10,18 @@
    [clojure.main])
   (:gen-class))
 
-(defn repl [reader]
-  (clojure.main/repl
-   :prompt (fn [])
-   :read (clj-repl-read reader)))
-
 (defn new-repl []
   (with-readline-input-stream (local-clj-service/create)
     (clojure.main/repl :prompt (fn []))))
 
-(defn -main []
-  #_(let [reader (line-reader (local-clj-service/create))]
-    (repl reader))
-
-  (new-repl)
+(defn -main [& args]
+  (let [reader (line-reader (local-clj-service/create))]
+    (clojure.main/repl
+     :prompt (fn [])
+     :read (clj-repl-read reader)))
   
   #_(let [repl-env (nash/repl-env)]
     (lr/with-readline-input-stream (cljs-service/create {:repl-env repl-env})
       (cljs-repl/repl repl-env :prompt (fn []))))
-  
-
-  
-
   
   )
