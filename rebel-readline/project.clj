58a09b205bae4b96bff7f633ff3bb4fe708cf426
Bruce Hauman <bhauman@gmail.com>
2018-02-09 18:50:08 -0500

    only rely on needed jline dependencies
    fixes #65

--- rebel-readline/project.clj
+++ rebel-readline/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.jline/jline "3.5.1"]
+                 [org.jline/jline-reader "3.5.1"]
+                 [org.jline/jline-terminal "3.5.1"]                 
                  [cljfmt "0.5.7"]         ;; depends on tools reader
                  [compliment "0.3.5"]]
 
