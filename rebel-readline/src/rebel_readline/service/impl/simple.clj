ea9da7966f0161f8c9910a7f45057e81025defec
Bruce Hauman <bhauman@gmail.com>
2018-02-11 08:03:29 -0500

    the simple service is just an atom with the default config

--- rebel-readline/src/rebel_readline/service/impl/simple.clj
+++ rebel-readline/src/rebel_readline/service/impl/simple.clj
@@ -2,28 +2,16 @@
   (:require
    [rebel-readline.service.core :as core]))
 
-;; The simplest possible service that you can use to get
-;; rebel readline working
-
-;; overide the :prompt option with a fn returns a proper
-;; prompt with the current namespace
-
-(defn create* [options]
-  (let [config-atom
-        (atom (merge
-               {:prompt (fn [] "clj=> ")}
-               options))]
-    (reify
-      clojure.lang.IDeref
-      (deref [_] @config-atom)
-      clojure.lang.IAtom
-      (swap  [_ f] (swap! config-atom f))
-      (swap  [_ f a] (swap! config-atom f a))
-      (swap  [_ f a b] (swap! config-atom f a b))
-      (swap  [_ f a b args] (swap! config-atom f a b args))
-      (reset [_ a] (reset! config-atom a)))))
-
 (defn create
+  "A very simple service that you can use to get rebel readline
+  working without any introspecting functionality (doc, source, appropos,
+  completion, eval).
+  
+  It's best overide the :prompt option with a fn returns a proper
+  prompt with the current namespace."
   ([] (create nil))
   ([options]
-   (create* (merge core/default-config options))))
+   (atom (merge
+          {:prompt (fn [] "clj=> ")}
+          core/default-config
+          options))))
