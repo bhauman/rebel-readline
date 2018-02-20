1c7b517de3f8a6532b1823bf8352f58dffa8bc9a
Bruce Hauman <bhauman@gmail.com>
2018-02-19 20:07:36 -0500

    fix tests

--- rebel-readline/test/rebel_readline/clojure/line_reader_test.clj
+++ rebel-readline/test/rebel_readline/clojure/line_reader_test.clj
@@ -1,6 +1,6 @@
-(ns rebel-readline.service.core-test
+(ns rebel-readline.clojure.line-reader-test
   (:require
-   [rebel-readline.service.core :as core]
+   [rebel-readline.clojure.line-reader :as core]
    [clojure.test :refer [deftest is are testing]]))
 
 (deftest default-accept-line-test
@@ -13,15 +13,15 @@
   (is (not (core/default-accept-line " \"2345" 2)))
   (is (not (core/default-accept-line " \"2345" 5)))
   (is (not (core/default-accept-line " \"2345" 6)))
-  
+
   (is (not (core/default-accept-line " \"2345\"" 2)))
   (is (not (core/default-accept-line " \"2345\"" 5)))
   (is (not (core/default-accept-line " \"2345\"" 6)))
   (is (core/default-accept-line " \"2345\"78" 7))
-  (is (core/default-accept-line " \"2345\"78" 8))   
-  
+  (is (core/default-accept-line " \"2345\"78" 8))
+
   ;; don't accept a line if there is an imcomplete form at the end
   ;; TODO not sure about this behavior
   #_(is (not (core/default-accept-line "()(" 2)))
-  
+
   )
