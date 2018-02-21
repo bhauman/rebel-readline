8eae0fef9b6329657ed10fcd4633c59d2c79144d
Bruce Hauman <bhauman@gmail.com>
2018-02-21 10:00:38 -0500

    change rebel cljs project version

--- rebel-readline-cljs/project.clj
+++ rebel-readline-cljs/project.clj
@@ -1,4 +1,4 @@
-(defproject rebel-readline-cljs "0.1.0-SNAPSHOT"
+(defproject rebel-readline-cljs "0.1.1-SNAPSHOT"
   :description "A rebel readline service for ClojureScript"
   :url "https://github.com/bhauman/rebel-readline"
   :license {:name "Eclipse Public License"
