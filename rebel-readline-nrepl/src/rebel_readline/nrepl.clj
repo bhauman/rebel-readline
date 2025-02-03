95a544828a5562876d68acfb411cf41ee2ef7733
Bruce Hauman <bhauman@gmail.com>
2025-02-03 17:34:40 -0600

    update spelling of :tools/usage

--- rebel-readline-nrepl/src/rebel_readline/nrepl.clj
+++ rebel-readline-nrepl/src/rebel_readline/nrepl.clj
@@ -3,9 +3,4 @@
   (:require [rebel-readline.nrepl.main :as main]))
 
 (defn connect [config]
-  "Connects to an nREPL server and launches a Rebel Readline REPL
-   Takes the following options:
-
-   :host string host for the nREPL server
-   :port numeric port for the nREPL server"
   (main/start-repl (or config {})))
