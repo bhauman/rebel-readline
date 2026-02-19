cc71d8a01d26ec117c7815027d2e7e3b67026891
Michiel Borkent <michielborkent@gmail.com>
2026-02-19 21:09:30 +0100

    wip

--- rebel-readline-cljs/src/rebel_readline/cljs/main.clj
+++ rebel-readline-cljs/src/rebel_readline/cljs/main.clj
@@ -1,10 +1,10 @@
 (ns rebel-readline.cljs.main
   (:require
-   [cljs.repl.nashorn :as nash]
+   [cljs.repl.node :as node]
    [rebel-readline.cljs.repl :as cljs-repl]
    [rebel-readline.core :as core]))
 
-;; TODO need ot bring this in line with cljs.main
+;; TODO need to bring this in line with cljs.main
 (defn -main [& args]
-  (let [repl-env (nash/repl-env)]
+  (let [repl-env (node/repl-env)]
     (cljs-repl/repl repl-env)))
